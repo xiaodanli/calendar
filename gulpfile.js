79f85726f238141ab570f6a15a2a38e7771ec716
李晓丹 <lixd@didi-zones.com>
2016-05-12 18:31:31 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var del = require('del');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var uglifycss = require('gulp-uglifycss');
+var runSequence = require('run-sequence');
+
+// clear old file
+gulp.task('clean', function() {
+  del.sync(['dist/*']);
+});
+
+// scripts
+gulp.task('scripts', function() {
+    return gulp.src('src/assets/js/calendar.js')
+        .pipe(uglify())    //压缩
+        .pipe(rename('calendar.min.js'))
+        .pipe(gulp.dest('dist/'));  //输出
+});
+
+// CSS
+gulp.task('csss', function() {
+    return gulp.src('src/assets/css/calendar.css')
+        .pipe(uglifycss())
+        .pipe(rename('calendar.min.css'))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', function() {
+    runSequence('clean', ['scripts', 'csss']);
+});
